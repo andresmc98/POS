eed23100dd2c361c271d569d268b86b5890ae860
andresmc98 <jossean98@gmail.com>
2024-03-15 14:36:33 -0700

    Unit of work and repository base

---
+++ POS.Infrastructure/Persistence/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace POS.Infrastructure.Persistence.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        //Declaracion o matricula de nuestras interfaces a nivel de repository
+
+        ICategoryRepository Category {  get; }
+
+        void SaveChanges();
+
+        Task SaveChangesAsync();
+    }
+}
---
+++ POS.Infrastructure/Persistence/Repositories/CategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using POS.Domain.Entities;
+using POS.Infrastructure.Persistence.Contexts;
+using POS.Infrastructure.Persistence.Interfaces;
+
+namespace POS.Infrastructure.Persistence.Repositories
+{
+    public class CategoryRepository : GenericRepository<Category>, ICategoryRepository
+    {
+        private readonly POSContext _context;
+
+        public CategoryRepository(POSContext context)
+        {
+            _context = context; 
+        }
+    }
+}
